d896ca9e50135b31e18e02c73121308a96cd8165
zmsxxd-ustc <zmsxxd@mail.ustc.edu.cn>
2024-08-03 14:41:18 +0800

    Update test_tpt_cocoop.sh

--- scripts/test_tpt_cocoop.sh
+++ scripts/test_tpt_cocoop.sh
@@ -9,6 +9,6 @@ arch_t=/model/zmsxxd/RN50/ViT-L-14.pt #ViT-L-14.pt
 # arch=ViT-B/16
 bs=64
 
-python ./tpt_classification.py ${data_root} --test_sets ${testsets} \
+python ./mgpd.py ${data_root} --test_sets ${testsets} \
 --arch ${arch} --arch_t ${arch_t} -b ${bs} --gpu 0 \
 --tpt --cocoop --lr 0.01 --BETA 0.9 --n_ctx 4
